889bd8108ae0371716ed117298a33e3948509cd9
Tristan Seligmann <mithrandi@mithrandi.net>
2017-06-27 23:50:52 +0200

    Fix dashboard.

--- grafana/main.dashboard.py
+++ grafana/main.dashboard.py
@@ -1,32 +1,45 @@
 import grafanalib.core as G
 
 dashboard = G.Dashboard(
-    title=u'UCDAPI',
+    title='UCDAPI',
     rows=[
         G.Row(panels=[
             G.Graph(
-                title=u'RPS',
-                dataSource=u'prometheus',
+                title='RPS',
+                dataSource='prometheus',
                 targets=[
                     G.Target(
-                        expr=u'sum(irate(http_request_duration_seconds_count{service="ucdapi"}[1m])) by (status_code, method)',
+                        expr='sum(irate(http_request_duration_seconds_count{service="ucdapi"}[1m])) by (status_code, method)',
                     ),
                 ],
                 yAxes=[
                     G.YAxis(format=G.OPS_FORMAT),
                     G.YAxis(format=G.SHORT_FORMAT),
                 ]),
+        ]),
+        G.Row(panels=[
             G.Graph(
-                title=u'Latency',
-                dataSource=u'prometheus',
+                title='Latency 0.9q',
+                dataSource='prometheus',
                 targets=[
                     G.Target(
-                        expr=u'http_request_duration_seconds{quantile="0.9",status_code="200",service="ucdapi"}*1000',
-                        legendFormat=u'{{pod}} 0.9 q',
+                        expr='http_request_duration_seconds{quantile="0.9",status_code="200",service="ucdapi"}*1000',
+                        legendFormat='{{pod}} 0.9 q',
+                        refId='A',
                     ),
+                ],
+                yAxes=[
+                    G.YAxis(format=G.MILLISECONDS_FORMAT),
+                    G.YAxis(format=G.SHORT_FORMAT, show=False),
+                ]),
+            G.Graph(
+                title='Latency 0.99q',
+                dataSource='prometheus',
+                targets=[
                     G.Target(
-                        expr=u'http_request_duration_seconds{quantile="0.99",status_code="200",service="ucdapi"}*1000',
-                        legendFormat=u'{{pod}} 0.99 q',
+                        expr='http_request_duration_seconds{quantile="0.99",status_code="200",service="ucdapi"}*1000',
+                        legendFormat='{{pod}} 0.99 q',
+                        refId='A',
                     ),
                 ],
                 yAxes=[
